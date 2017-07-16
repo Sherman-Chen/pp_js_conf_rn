572b2023a7e9a2a12e9187cfe7d5441fd53f2860
Chen <shermchen@lm-sjn-21011561.paypalcorp.com>
2017-07-15 17:28:23 -0700

    i need to review import/export

--- utils/services.js
+++ utils/services.js
@@ -4,26 +4,24 @@ const yelp = require('yelp-fusion');
 const clientId = '6diInag_AaJ5DW953SH5sQ';
 const clientSecret = 'tKCbvczdQet4l1mFNCJTXpljDpN5SAj4bfseg0hjLTwNnq0bFJJcTVr8Gj5CzLCI';
 
-const yelpSearch = (query, location) => {
-  return yelp.accessToken(clientId, clientSecret).then(response => {
-    const client = yelp.client(response.jsonBody.access_token);
-    const searchRequest = {
-      term: query,
-      location: location,
-      limit: 10,
-      radius: 20000
-    };
+export default const service = {
+    yelpSearch(query, location) {
+        return yelp.accessToken(clientId, clientSecret).then(response => {
+          const client = yelp.client(response.jsonBody.access_token);
+          const searchRequest = {
+            term: query,
+            location: location,
+            limit: 10,
+            radius: 20000
+          };
 
-    client.search(searchRequest).then(response => {
-      console.log(response.jsonBody.businesses);
-      console.log(Object.keys(response.jsonBody.businesses).length);
-      return response.jsonBody.businesses;
-    });
-  }).catch(e => {
-    console.log(e);
-  });
+          client.search(searchRequest).then(response => {
+            console.log(response.jsonBody.businesses);
+            console.log(Object.keys(response.jsonBody.businesses).length);
+            return response.jsonBody.businesses;
+          });
+        }).catch(e => {
+          console.log(e);
+        });
+    }
 }
-
-module.exports = {
-  yelpSearch
-};
